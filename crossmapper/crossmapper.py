172bd6427f95de095dd3015122601b7f287cf3ff
J.F.J. Laros <J.F.J.Laros@lumc.nl>
2019-05-03 20:08:42 +0200

    Reorganised by object orientation.

--- crossmapper/crossmapper.py
+++ crossmapper/crossmapper.py
@@ -34,39 +34,36 @@ def _nearest_boundary(coordinate, boundaries):
     return insertion_point
 
 
-def _nearest_exon(coordinate, boundaries):
-    return _nearest_boundary(coordinate, boundaries) // 2;
+def _nearest_locus(coordinate, boundaries):
+    return _nearest_boundary(coordinate, boundaries) // 2
 
 
-# TODO: From position and offset to nearest exon.
-
-
-def _cut(coordinate, exons):
-    """Divide a list of exons, cutting one of the exons in two.
+def _cut(coordinate, loci):
+    """Divide a list of loci, cutting one of the loci in two.
 
     :arg int coordinate: Coordinate.
-    :arg list exons: List of exons.
+    :arg list loci: List of loci.
 
-    :returns tuple: {exons} before coordinate, {exons} after coordinate.
+    :returns tuple: {loci} before coordinate, {loci} after coordinate.
     """
-    exon = _nearest_exon(coordinate, exons)
+    locus = _nearest_locus(coordinate, loci)
 
     return (
-        exons[:exon] + [(exons[exon][0], coordinate)],
-        [(coordinate, exons[exon][1])] + exons[exon + 1:])
+        loci[:locus] + [(loci[locus][0], coordinate)],
+        [(coordinate, loci[locus][1])] + loci[locus + 1:])
 
 
-def _negate(position):
-    return (-position[0], -position[1])
+#def _negative(position):
+#    return (-position[0], -position[1])
 
 
-def _offsets(exons, inverted=False):
-    """For each exon, calculate the length of the preceding exons.
+def _offsets(loci, inverted=False):
+    """For each locus, calculate the length of the preceding loci.
 
-    :arg list exons: List of exons.
-    :arg bool inverted: Direction of {exons}.
+    :arg list loci: List of loci.
+    :arg bool inverted: Direction of {loci}.
 
-    :returns list: List of cumulative exon lengths.
+    :returns list: List of cumulative locus lengths.
     """
     direction = 1
     if inverted:
@@ -74,109 +71,203 @@ def _offsets(exons, inverted=False):
 
     length = 0
     lengths = []
-    for exon in exons[::direction]:
+    for locus in loci[::direction]:
         lengths.append(length)
-        length += exon[1] - exon[0]
+        length += locus[1] - locus[0]
 
     return lengths[::direction]
 
 
-def _coordinate_to_genomic(coordinate):
+def to_position(coordinate):
     return coordinate + 1
 
 
-def _genomic_to_coordinate(position):
+def to_coordinate(position):
     return position - 1
 
 
-def _coordinate_to_locus(coordinate, locus, inverted=False):
-    """Convert a coordinate to a position relative to a locus.
-
-    :arg int coordinate: Coordinate.
-    :arg tuple locus: Locus.
-    :arg bool inverted: Direction of {locus}.
-
-    :returns tuple: Position relative to {locus}.
-    """
-    if inverted:
-        if coordinate >= locus[1]:
-            return 1, locus[1] - coordinate - 1
-        if coordinate < locus[0]:
-            return locus[1] - locus[0], locus[0] - coordinate
-        return locus[1] - coordinate, 0
-
-    if coordinate < locus[0]:
-        return 1, coordinate - locus[0]
-    if coordinate >= locus[1]:
-        return locus[1] - locus[0], coordinate - locus[1] + 1
-    return coordinate - locus[0] + 1, 0
-
-
-def _locus_to_coordinate(position, locus, inverted=False):
-    """Convert a position relative to a locus to a coordinate.
-
-    :arg int position: Position.
-    :arg tuple locus: Locus.
-    :arg bool inverted: Direction of {locus}.
-
-    :returns int: Coordinate.
-    """
-    if inverted:
-        return locus[1] - position[0] - position[1]
-    return locus[0] + position[0] + position[1] - 1
+#def _coordinate_to_locus(coordinate, locus, inverted=False):
+#    """Convert a coordinate to a position relative to a locus.
+#
+#    :arg int coordinate: Coordinate.
+#    :arg tuple locus: Locus.
+#    :arg bool inverted: Direction of {locus}.
+#
+#    :returns tuple: Position relative to {locus}.
+#    """
+#    if inverted:
+#        if coordinate >= locus[1]:
+#            return 1, locus[1] - coordinate - 1
+#        if coordinate < locus[0]:
+#            return locus[1] - locus[0], locus[0] - coordinate
+#        return locus[1] - coordinate, 0
+#
+#    if coordinate < locus[0]:
+#        return 1, coordinate - locus[0]
+#    if coordinate >= locus[1]:
+#        return locus[1] - locus[0], coordinate - locus[1] + 1
+#    return coordinate - locus[0] + 1, 0
 
 
-def _coordinate_to_exon(coordinate, exon, offset, inverted=False):
-    """Convert a coordinate to a position relative to an exon.
+#def _locus_to_coordinate(position, locus, inverted=False):
+#    """Convert a position relative to a locus to a coordinate.
+#
+#    :arg int position: Position.
+#    :arg tuple locus: Locus.
+#    :arg bool inverted: Direction of {locus}.
+#
+#    :returns int: Coordinate.
+#    """
+#    if inverted:
+#        return locus[1] - position[0] - position[1]
+#    return locus[0] + position[0] + position[1] - 1
 
-    :arg int coordinate: Coordinate.
-    :arg tuple exon: Exon.
-    :arg int offset: Offset of {exon}.
-    :arg bool inverted: Direction of {exon}.
 
-    :returns tuple: Position relative to {exon}.
-    """
-    locus = _coordinate_to_locus(coordinate, exon, inverted)
+#def _coordinate_to_exon(coordinate, exon, offset, inverted=False):
+#    """Convert a coordinate to a position relative to an exon.
+#
+#    :arg int coordinate: Coordinate.
+#    :arg tuple exon: Exon.
+#    :arg int offset: Offset of {exon}.
+#    :arg bool inverted: Direction of {exon}.
+#
+#    :returns tuple: Position relative to {exon}.
+#    """
+#    locus = _coordinate_to_locus(coordinate, exon, inverted)
+#
+#    return locus[0] + offset, locus[1]
 
-    return locus[0] + offset, locus[1]
+
+#def _exon_to_coordinate(position, exon, offset, inverted=False):
+#    """Convert a position relative to an exon to a coordinate.
+#
+#    :arg int position: Position.
+#    :arg tuple exon: Exon.
+#    :arg int offset: Offset of {exon}.
+#    :arg bool inverted: Direction of {exon}.
+#
+#    :returns int: Coordinate.
+#    """
+#    return _locus_to_coordinate(
+#        (position[0] - offset, position[1]), exon, inverted)
 
 
-def _exon_to_coordinate(position, exon, offset, inverted=False):
-    """Convert a position relative to an exon to a coordinate.
+class Locus(object):
+    def __init__(self, locus, inverted=False):
+        """Construct a Locus object.
 
-    :arg int position: Position.
-    :arg tuple exon: Exon.
-    :arg int offset: Offset of {exon}.
-    :arg bool inverted: Direction of {exon}.
+        :arg tuple locus: Locus coordinates.
+        :arg bool inverted: Orientation of {locus}.
+        """
+        self._locus = locus
+        self._inverted = inverted
 
-    :returns int: Coordinate.
-    """
-    return _locus_to_coordinate(
-        (position[0] - offset, position[1]), exon, inverted)
+    def to_position(self, coordinate):
+        """Convert a coordinate to a position.
+
+        :arg int coordinate: Coordinate.
+
+        :returns tuple: Position.
+        """
+        if self._inverted:
+            if coordinate >= self._locus[1]:
+                return 1, self._locus[1] - coordinate - 1
+            if coordinate < self._locus[0]:
+                return (
+                    self._locus[1] - self._locus[0],
+                    self._locus[0] - coordinate)
+            return self._locus[1] - coordinate, 0
+
+        if coordinate < self._locus[0]:
+            return 1, coordinate - self._locus[0]
+        if coordinate >= self._locus[1]:
+            return (
+                self._locus[1] - self._locus[0],
+                coordinate - self._locus[1] + 1)
+        return coordinate - self._locus[0] + 1, 0
+
+    def to_coordinate(self, position):
+        """Convert a position to a coordinate.
+
+        :arg int position: Position.
+
+        :returns int: Coordinate.
+        """
+        if self._inverted:
+            return self._locus[1] - position[0] - position[1]
+        return self._locus[0] + position[0] + position[1] - 1
+
+
+class MultiLocus(object):
+    def __init__(self, locus_list, inverted=False, negated=False):
+        """Construct a MultiLocus object.
+        :arg list locus_list: List of locus coordinates.
+        :arg bool inverted: Orientation of {locus}.
+        :arg bool negated: Change the sign of all positions.
+        """
+        self._inverted = inverted
+        self._negated = negated
+
+        self._loci = [Locus(locus, inverted) for locus in locus_list]
+        self._boundaries = sum(
+            [[locus[0], locus[1] - 1] for locus in locus_list], [])
+
+        self._offsets = _offsets(locus_list, inverted)
+        if inverted:
+            self._offsets = self._offsets[::-1]
+
+    def _sign(self, position):
+        if self._negated:
+            return -position[0], -position[1]
+        return position
+
+    def to_position(self, coordinate):
+        """Convert a coordinate to a position.
+
+        :arg int coordinate: Coordinate.
+
+        :returns tuple: Position.
+        """
+        index = _nearest_boundary(coordinate, self._boundaries) // 2
+        locus = self._loci[index].to_position(coordinate)
+
+        return self._sign((locus[0] + self._offsets[index], locus[1]))
+
+    def to_coordinate(self, position):
+        """Convert a position to a coordinate.
+
+        :arg int position: Position.
+
+        :returns int: Coordinate.
+        """
+        position_ = self._sign(position)
+
+        index = min(
+            len(self._offsets),
+            max(0, bisect_left(self._offsets, position_[0]) - 1))
+
+        return self._loci[index].to_coordinate(
+            (position_[0] - self._offsets[index], position_[1]))
 
 
 class Crossmap(object):
-    def __init__(self, locus, exons=None, cds=None, inverted=False):
-        self._inverted = inverted
-        self._locus = locus
-        self._exons = exons
-        self._cds = cds
-        self._inverted = inverted
+    def __init__(self, locus=None, exons=None, cds=None, inverted=False):
+        self._locus = None
+        self._noncoding = None
+        self._coding = None
 
-        self._boundaries = None
-        self._offsets = None
-        if self._exons:
-            self._boundaries = sum([e[0], e[1] - 1] for e in self._exons, [])
-            self._offsets = _offsets(self._exons)
+        if locus:
+            self._locus = Locus(locus, inverted)
+        if exons:
+            self._noncoding = MultiLocus(exons, inverted)
+        if cds:
+            utr5, tail = cut(cds[0], self._exons)
+            coding, utr3 = cut(cds[1], tail)
 
-        self._cds_exons = None
-        self._cds_offsets = None
-        if self._cds:
-            utr5, tail = cut(self._cds[0], self._exons)
-            coding, utr3 = cut(self._cds[1], tail)
-            self._cds_exons = (utr5, coding, utr3)
-            self._cds_offsets = (
-                _offsets(utr5, True), _offsets(coding), _offsets(utr3))
+            self._coding = (
+                MultiLocus(utr5, inverted, True),
+                MultiLocus(coding, inverted),
+                MultiLocus(utr3, inverted))
 
     def coordinate_to_genomic(self, coordinate):
         """Convert a coordinate to a genomic position (g./m./o.).
@@ -221,7 +312,7 @@ class Crossmap(object):
 
         :returns tuple: Noncoding position.
         """
-        exon = _nearest_exon(coordinate, self._boundaries)
+        exon = _nearest_locus(coordinate, self._boundaries)
 
         return _coordinate_to_exon(
             coordinate, self._exons[exon], self._offsets[exon], self._inverted)
--- tests/test_crossmap.py
+++ tests/test_crossmap.py
@@ -1,9 +1,11 @@
 from crossmapper.crossmapper import (
-    _coordinate_to_exon,
-    _coordinate_to_locus,
-    _exon_to_coordinate,
-    _locus_to_coordinate,
+    #_coordinate_to_exon,
+    #_coordinate_to_locus,
+    #_exon_to_coordinate,
+    #_locus_to_coordinate,
     _offsets,
+    Locus,
+    MultiLocus
     )
 
 
@@ -14,9 +16,12 @@ _offsets_exon = [0, 3, 9, 14, 20]
 _offsets_exon_inverted = [19, 13, 8, 2, 0]
 
 
-def _test_invariant(f, x, f_i, y, args):
-    assert f(x, *args) == y
-    assert f_i(y, *args) == x
+#def _test_invariant(f, x, f_i, y, args):
+#    assert f(x, *args) == y
+#    assert f_i(y, *args) == x
+def _test_invariant(f, x, f_i, y):
+    assert f(x) == y
+    assert f_i(y) == x
 
 
 def test_offsets():
@@ -24,81 +29,171 @@ def test_offsets():
     assert _offsets(_exons, True) == _offsets_exon_inverted
 
 
-def test_locus():
-    locus = _exons[2]
-
-    _test_invariant(
-        _coordinate_to_locus, 29, _locus_to_coordinate, (1, -1), [locus])
-    _test_invariant(
-        _coordinate_to_locus, 30, _locus_to_coordinate, (1, 0), [locus])
-    _test_invariant(
-        _coordinate_to_locus, 31, _locus_to_coordinate, (2, 0), [locus])
-    _test_invariant(
-        _coordinate_to_locus, 33, _locus_to_coordinate, (4, 0), [locus])
-    _test_invariant(
-        _coordinate_to_locus, 34, _locus_to_coordinate, (5, 0), [locus])
-    _test_invariant(
-        _coordinate_to_locus, 35, _locus_to_coordinate, (5, 1), [locus])
-
-
-def test_locus_inverted():
-    locus = _exons[2]
-
-    _test_invariant(
-        _coordinate_to_locus, 35, _locus_to_coordinate, (1, -1), [locus, True])
-    _test_invariant(
-        _coordinate_to_locus, 34, _locus_to_coordinate, (1, 0), [locus, True])
-    _test_invariant(
-        _coordinate_to_locus, 33, _locus_to_coordinate, (2, 0), [locus, True])
-    _test_invariant(
-        _coordinate_to_locus, 31, _locus_to_coordinate, (4, 0), [locus, True])
-    _test_invariant(
-        _coordinate_to_locus, 30, _locus_to_coordinate, (5, 0), [locus, True])
-    _test_invariant(
-        _coordinate_to_locus, 29, _locus_to_coordinate, (5, 1), [locus, True])
+#def test_locus():
+#    locus = _exons[2]
+#
+#    _test_invariant(
+#        _coordinate_to_locus, 29, _locus_to_coordinate, (1, -1), [locus])
+#    _test_invariant(
+#        _coordinate_to_locus, 30, _locus_to_coordinate, (1, 0), [locus])
+#    _test_invariant(
+#        _coordinate_to_locus, 31, _locus_to_coordinate, (2, 0), [locus])
+#    _test_invariant(
+#        _coordinate_to_locus, 33, _locus_to_coordinate, (4, 0), [locus])
+#    _test_invariant(
+#        _coordinate_to_locus, 34, _locus_to_coordinate, (5, 0), [locus])
+#    _test_invariant(
+#        _coordinate_to_locus, 35, _locus_to_coordinate, (5, 1), [locus])
 
 
-def test_exon():
-    exon = 2
+def test_Locus():
+    locus = Locus(_exons[2])
 
-    _test_invariant(
-        _coordinate_to_exon, 29, _exon_to_coordinate, (10, -1),
-        [_exons[exon], _offsets_exon[exon]])
-    _test_invariant(
-        _coordinate_to_exon, 30, _exon_to_coordinate, (10, 0),
-        [_exons[exon], _offsets_exon[exon]])
-    _test_invariant(
-        _coordinate_to_exon, 31, _exon_to_coordinate, (11, 0),
-        [_exons[exon], _offsets_exon[exon]])
-    _test_invariant(
-        _coordinate_to_exon, 33, _exon_to_coordinate, (13, 0),
-        [_exons[exon], _offsets_exon[exon]])
-    _test_invariant(
-        _coordinate_to_exon, 34, _exon_to_coordinate, (14, 0),
-        [_exons[exon], _offsets_exon[exon]])
-    _test_invariant(
-        _coordinate_to_exon, 35, _exon_to_coordinate, (14, 1),
-        [_exons[exon], _offsets_exon[exon]])
+    _test_invariant(locus.to_position, 29, locus.to_coordinate, (1, -1))
+    _test_invariant(locus.to_position, 30, locus.to_coordinate, (1, 0))
+    _test_invariant(locus.to_position, 31, locus.to_coordinate, (2, 0))
+    _test_invariant(locus.to_position, 33, locus.to_coordinate, (4, 0))
+    _test_invariant(locus.to_position, 34, locus.to_coordinate, (5, 0))
+    _test_invariant(locus.to_position, 35, locus.to_coordinate, (5, 1))
 
 
-def test_exon_inverted():
-    exon = 2
+#def test_locus_inverted():
+#    locus = _exons[2]
+#
+#    _test_invariant(
+#        _coordinate_to_locus, 35, _locus_to_coordinate, (1, -1), [locus, True])
+#    _test_invariant(
+#        _coordinate_to_locus, 34, _locus_to_coordinate, (1, 0), [locus, True])
+#    _test_invariant(
+#        _coordinate_to_locus, 33, _locus_to_coordinate, (2, 0), [locus, True])
+#    _test_invariant(
+#        _coordinate_to_locus, 31, _locus_to_coordinate, (4, 0), [locus, True])
+#    _test_invariant(
+#        _coordinate_to_locus, 30, _locus_to_coordinate, (5, 0), [locus, True])
+#    _test_invariant(
+#        _coordinate_to_locus, 29, _locus_to_coordinate, (5, 1), [locus, True])
+
+
+def test_Locus_inverted():
+    locus = Locus(_exons[2], True)
+
+    _test_invariant(locus.to_position, 35, locus.to_coordinate, (1, -1))
+    _test_invariant(locus.to_position, 34, locus.to_coordinate, (1, 0))
+    _test_invariant(locus.to_position, 33, locus.to_coordinate, (2, 0))
+    _test_invariant(locus.to_position, 31, locus.to_coordinate, (4, 0))
+    _test_invariant(locus.to_position, 30, locus.to_coordinate, (5, 0))
+    _test_invariant(locus.to_position, 29, locus.to_coordinate, (5, 1))
+
+
+#def test_exon():
+#    exon = 2
+#
+#    _test_invariant(
+#        _coordinate_to_exon, 29, _exon_to_coordinate, (10, -1),
+#        [_exons[exon], _offsets_exon[exon]])
+#    _test_invariant(
+#        _coordinate_to_exon, 30, _exon_to_coordinate, (10, 0),
+#        [_exons[exon], _offsets_exon[exon]])
+#    _test_invariant(
+#        _coordinate_to_exon, 31, _exon_to_coordinate, (11, 0),
+#        [_exons[exon], _offsets_exon[exon]])
+#    _test_invariant(
+#        _coordinate_to_exon, 33, _exon_to_coordinate, (13, 0),
+#        [_exons[exon], _offsets_exon[exon]])
+#    _test_invariant(
+#        _coordinate_to_exon, 34, _exon_to_coordinate, (14, 0),
+#        [_exons[exon], _offsets_exon[exon]])
+#    _test_invariant(
+#        _coordinate_to_exon, 35, _exon_to_coordinate, (14, 1),
+#        [_exons[exon], _offsets_exon[exon]])
+
+
+def test_MultiLocus():
+    multi_locus = MultiLocus(_exons)
 
     _test_invariant(
-        _coordinate_to_exon, 35, _exon_to_coordinate, (9, -1),
-        [_exons[exon], _offsets_exon_inverted[exon], True])
+        multi_locus.to_position, 29, multi_locus.to_coordinate, (10, -1))
     _test_invariant(
-        _coordinate_to_exon, 34, _exon_to_coordinate, (9, 0),
-        [_exons[exon], _offsets_exon_inverted[exon], True])
+        multi_locus.to_position, 30, multi_locus.to_coordinate, (10, 0))
     _test_invariant(
-        _coordinate_to_exon, 33, _exon_to_coordinate, (10, 0),
-        [_exons[exon], _offsets_exon_inverted[exon], True])
+        multi_locus.to_position, 31, multi_locus.to_coordinate, (11, 0))
     _test_invariant(
-        _coordinate_to_exon, 31, _exon_to_coordinate, (12, 0),
-        [_exons[exon], _offsets_exon_inverted[exon], True])
+        multi_locus.to_position, 33, multi_locus.to_coordinate, (13, 0))
     _test_invariant(
-        _coordinate_to_exon, 30, _exon_to_coordinate, (13, 0),
-        [_exons[exon], _offsets_exon_inverted[exon], True])
+        multi_locus.to_position, 34, multi_locus.to_coordinate, (14, 0))
     _test_invariant(
-        _coordinate_to_exon, 29, _exon_to_coordinate, (13, 1),
-        [_exons[exon], _offsets_exon_inverted[exon], True])
+        multi_locus.to_position, 35, multi_locus.to_coordinate, (14, 1))
+
+
+#def test_exon_inverted():
+#    exon = 2
+#
+#    _test_invariant(
+#        _coordinate_to_exon, 35, _exon_to_coordinate, (9, -1),
+#        [_exons[exon], _offsets_exon_inverted[exon], True])
+#    _test_invariant(
+#        _coordinate_to_exon, 34, _exon_to_coordinate, (9, 0),
+#        [_exons[exon], _offsets_exon_inverted[exon], True])
+#    _test_invariant(
+#        _coordinate_to_exon, 33, _exon_to_coordinate, (10, 0),
+#        [_exons[exon], _offsets_exon_inverted[exon], True])
+#    _test_invariant(
+#        _coordinate_to_exon, 31, _exon_to_coordinate, (12, 0),
+#        [_exons[exon], _offsets_exon_inverted[exon], True])
+#    _test_invariant(
+#        _coordinate_to_exon, 30, _exon_to_coordinate, (13, 0),
+#        [_exons[exon], _offsets_exon_inverted[exon], True])
+#    _test_invariant(
+#        _coordinate_to_exon, 29, _exon_to_coordinate, (13, 1),
+#        [_exons[exon], _offsets_exon_inverted[exon], True])
+
+
+def test_MultiLocus_inverted():
+    multi_locus = MultiLocus(_exons, True)
+
+    _test_invariant(
+        multi_locus.to_position, 35, multi_locus.to_coordinate, (9, -1))
+    _test_invariant(
+        multi_locus.to_position, 34, multi_locus.to_coordinate, (9, 0))
+    _test_invariant(
+        multi_locus.to_position, 33, multi_locus.to_coordinate, (10, 0))
+    _test_invariant(
+        multi_locus.to_position, 31, multi_locus.to_coordinate, (12, 0))
+    _test_invariant(
+        multi_locus.to_position, 30, multi_locus.to_coordinate, (13, 0))
+    _test_invariant(
+        multi_locus.to_position, 29, multi_locus.to_coordinate, (13, 1))
+
+
+def test_MultiLocus_negated():
+    multi_locus = MultiLocus(_exons, False, True)
+
+    _test_invariant(
+        multi_locus.to_position, 29, multi_locus.to_coordinate, (-10, 1))
+    _test_invariant(
+        multi_locus.to_position, 30, multi_locus.to_coordinate, (-10, 0))
+    _test_invariant(
+        multi_locus.to_position, 31, multi_locus.to_coordinate, (-11, 0))
+    _test_invariant(
+        multi_locus.to_position, 33, multi_locus.to_coordinate, (-13, 0))
+    _test_invariant(
+        multi_locus.to_position, 34, multi_locus.to_coordinate, (-14, 0))
+    _test_invariant(
+        multi_locus.to_position, 35, multi_locus.to_coordinate, (-14, -1))
+
+
+def test_MultiLocus_inverted_negated():
+    multi_locus = MultiLocus(_exons, True, True)
+
+    _test_invariant(
+        multi_locus.to_position, 35, multi_locus.to_coordinate, (-9, 1))
+    _test_invariant(
+        multi_locus.to_position, 34, multi_locus.to_coordinate, (-9, 0))
+    _test_invariant(
+        multi_locus.to_position, 33, multi_locus.to_coordinate, (-10, 0))
+    _test_invariant(
+        multi_locus.to_position, 31, multi_locus.to_coordinate, (-12, 0))
+    _test_invariant(
+        multi_locus.to_position, 30, multi_locus.to_coordinate, (-13, 0))
+    _test_invariant(
+        multi_locus.to_position, 29, multi_locus.to_coordinate, (-13, -1))
