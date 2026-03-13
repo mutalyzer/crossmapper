87f507d0b8916c5764d448ad467d74dd89d82fda
Xiaoyun Liu <x.liu@lumc.nl>
2026-03-13 11:38:43 +0100

    Add missing tests

--- tests/test_crossmapper.py
+++ tests/test_crossmapper.py
@@ -106,7 +106,6 @@ def test_NonCoding_degenerate():
         4,
         [
             {"position": 1, "offset": 0, "region": "u"},
-            {"position": 0, "offset": -1, "region": "u"},
             {"position": 1, "offset": -1, "region": ""},
         ],
     )
@@ -117,8 +116,7 @@ def test_NonCoding_degenerate():
         72,
         [
             {"position": 1, "offset": 0, "region": "d"},
-            {"position": 0, "offset": 1, "region": "d"},
-            {"position": 22, "offset": 1, "region": ""},
+            {"position": 23, "offset": 0, "region": ""},
         ],
     )
 
@@ -132,8 +130,8 @@ def test_NonCoding_inverted_degenerate():
         crossmap.noncoding_to_coordinate,
         72,
         [
-            {"position": 1, "offset": 0, "region": "u"},
-            {"position": 1, "offset": -1, "region": ""},],
+            {"position": 1, "offset": -1, "region": ""},
+            {"position": 1, "offset": 0, "region": "u"},],
     )
 
     # Boundary between downstream and transcript.
@@ -463,22 +461,19 @@ def test_Coding_degenerate():
         9,
         [
             {"position": 1, "offset": 0, "region": "u"},
-            {"position": 2, "offset": 1, "region": "u"},
-            {"position": 0, "offset": -1, "region": "u"},
-            {"position": 1, "offset": -1, "region": "-"},
+            {"position": 2, "offset": 0, "region": "-"},
             {"position": 1, "offset": -2, "region": ""},
+            {"position": 1, "offset": -10, "region": "*"},
         ],
     )
     degenerate_equal(
         crossmap.coding_to_coordinate,
         20,
         [
-            {"position": 1, "offset": 0, "region": "d"},
-            {"position": 8, "offset": -7, "region": "d"},
-            {"position": 0, "offset": -1, "region": "d"},
-            {"position": 8, "offset": 2, "region": ""},
-            {"position": 2, "offset": 0, "region": "*"},
             {"position": 1, "offset": 1, "region": "*"},
+            {"position": 2, "offset": 0, "region": "*"},
+            {"position": 8, "offset": 2, "region": ""},
+            {"position": 1, "offset": 10, "region": "-"},
         ],
     )
 
