08f22617cdc4628656e3a276767e40c4c525fa72
Xiaoyun Liu <x.liu@lumc.nl>
2026-06-25 15:44:45 +0200

    Test importlib_metadata to avoid mypy error from python v3.10 and v3.11.

--- mutalyzer_crossmapper/__init__.py
+++ mutalyzer_crossmapper/__init__.py
@@ -1,4 +1,5 @@
-from importlib.metadata import metadata
+# from importlib.metadata import metadata
+from importlib_metadata import metadata
 
 from .crossmapper import Coding, Genomic, NonCoding, GenomicPoint, NonCodingPoint, CodingPoint, ProteinPoint
 from .location import nearest_location
