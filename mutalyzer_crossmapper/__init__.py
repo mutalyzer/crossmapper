3f06bbf02e2d1f9ff81db77472af563b24e6812c
Mihai Lefter <mihai.lefter@gmail.com>
2026-09-04 11:58:08 +0200

    Export Coord and Point from the package root.

--- mutalyzer_crossmapper/__init__.py
+++ mutalyzer_crossmapper/__init__.py
@@ -2,8 +2,8 @@ from importlib.metadata import metadata
 
 from .crossmapper import Coding, Genomic, NonCoding, GenomicPoint, NonCodingPoint, CodingPoint, ProteinPoint
 from .location import _nearest_location
-from .locus import Locus
-from .multi_locus import MultiLocus
+from .locus import Coord, Locus
+from .multi_locus import MultiLocus, Point
 
 
 def _get_metadata(name: str) -> str:
